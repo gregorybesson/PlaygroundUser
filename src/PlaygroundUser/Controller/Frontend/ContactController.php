20c031aa227c67fa6b677db281f39be87657dc8e
GrG <gregory.besson@adfab.fr>
2014-08-23 23:44:29 +0200

    contact form

---
+++ src/PlaygroundUser/Controller/Frontend/ContactController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace PlaygroundUser\Controller\Frontend;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ContactController extends AbstractActionController
+{
+    /**
+     *
+     */
+    protected $options;
+
+     /**
+     * @var mailService
+     */
+    protected $mailService;
+
+    public function indexAction()
+    {
+        $mailService = $this->getMailService();
+
+        $to = '';
+        $config = $this->getServiceLocator()->get('config');
+        if (isset($config['contact']['email'])) {
+            $to = $config['contact']['email'];
+        }
+
+        $form = $this->getServiceLocator()->get('playgrounduser_contact_form');
+        $form->setAttribute('method', 'post');
+
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost()->toArray();
+            $form->setData($data);
+            if ($form->isValid()) {
+                $from = $data['email'];
+                $subject= 'Contact : '.$data['object'];
+                $result = $mailService->createHtmlMessage($from, $to, $subject, 'playground-user/email/question', array('data' => $data));
+
+                if ($result) {
+                   return $this->redirect()->toRoute('frontend/contact/confirmation', array('channel' => $this->getEvent()->getRouteMatch()->getParam('channel')));
+                }
+            }
+        }
+
+        return new ViewModel(array(
+                'form' => $form,
+            )
+        );
+    }
+
+    public function confirmationAction()
+    {
+        return new ViewModel();
+    }
+
+    public function getMailService()
+    {
+        if (!$this->mailService) {
+            $this->mailService = $this->getServiceLocator()->get('playgroundgame_message');
+        }
+
+        return $this->mailService;
+    }
+
+    public function setMailService($mailService)
+    {
+        $this->mailService = $mailService;
+
+        return $this;
+    }
+
+    public function getOptions()
+    {
+        if (!$this->options) {
+            $this->setOptions($this->getServiceLocator()->get('playgroundcore_module_options'));
+        }
+
+        return $this->options;
+    }
+
+    public function setOptions($options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
